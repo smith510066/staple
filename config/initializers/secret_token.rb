cb5417474c0ebfddd93264da8c1d81dd867e6b08
unknown <smith510066@yahoo.com>
2012-09-08 08:09:23 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staple::Application.config.secret_token = 'ff0a14b24e3412856eb16c00f3f1bc4a9d79d895ee0a17b49d6790389687fc03176e19ae687765064daf2b868591d89104e32b20d30af9edfb0dc1e64b84fa5f'
